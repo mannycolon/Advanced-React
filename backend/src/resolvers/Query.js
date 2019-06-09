8ab3b42ec1aa11a65aae8dc1a8e5df707970898a
mannycolon <colonmanuel7@gmail.com>
2019-06-09 18:36:43 -0400

    Added itemsConnection query for pagination assitence

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -8,7 +8,20 @@ async function item(parent, args, context, info) {
   return context.prisma.item({ id: args.id })
 }
 
+async function itemsConnection(parent, args, context, info) {
+  const count = await context.prisma.itemsConnection({ where: args.where })
+    .aggregate()
+    .count()
+
+  return {
+    aggregate: {
+      count
+    }
+  }
+}
+
 module.exports = {
   items,
+  itemsConnection,
   item
 };
