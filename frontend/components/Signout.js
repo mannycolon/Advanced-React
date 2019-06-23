528b2fb802bedbf1e31a1c8413dbd1cb9d2baf9b
mannycolon <colonmanuel7@gmail.com>
2019-06-22 21:16:32 -0400

    Added signout UI

---
+++ frontend/components/Signout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import { CURRENT_USER_QUERY } from './User'
+
+const SIGN_OUT_MUTATION = gql`
+  mutation {
+    signout {
+      message
+    }
+  }
+`
+
+const Signout = () => (
+  <Mutation
+    mutation={SIGN_OUT_MUTATION}
+    refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+  >
+    {(signout) => <button onClick={signout}>Sign Out</button>}
+  </Mutation>
+);
+
+export default Signout;
