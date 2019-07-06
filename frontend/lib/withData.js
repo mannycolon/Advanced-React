a2a4b5a6bb49e02162b18d191fbb53b306026998
mannycolon <colonmanuel7@gmail.com>
2019-07-05 20:30:12 -0400

    Added apollo local state for Cart. Now toggling Cart on & off

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,7 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart'
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -13,6 +14,26 @@ function createClient({ headers }) {
         headers,
       });
     },
+    // local state
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            // Read the cartOpen value from cache
+            const {cartOpen} = cache.readQuery({ query: LOCAL_STATE_QUERY })
+            // Write cart state to the opposite
+            const data = {
+              data: { cartOpen: !cartOpen }
+            }
+            cache.writeData(data)
+            return data;
+          }
+        }
+      },
+      defaults: {
+        cartOpen: true
+      }
+    }
   });
 }
 
