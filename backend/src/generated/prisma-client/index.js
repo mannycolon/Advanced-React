f19cb0c86c61d459a5d10a204818f2458efa3a94
mannycolon <colonmanuel7@gmail.com>
2019-07-21 00:48:04 -0400

    updates

--- backend/src/generated/prisma-client/index.js
+++ backend/src/generated/prisma-client/index.js
@@ -32,6 +32,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `${process.env["PRISMA_ENDPOINT"]}`
+  endpoint: `https://sick-fits-macp-e570d07844.herokuapp.com/sick-fits-prod/prod`,
+  secret: `${process.env["PRISMA_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
